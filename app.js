05f41ffee3d34387b6c5b1e658553dad4e063439
Adam-Harte <adamharte11@hotmail.co.uk>
2020-04-23 23:13:19 +0100

    named exports and imports added

---
+++ app.js
@@ -0,0 +1,5 @@
+//named imports
+import { age, logName } from './namedExport';
+
+console.log(age);
+logName();
---
+++ namedExport.js
@@ -0,0 +1,6 @@
+// named exports
+export const name = 'Adam';
+export const age = 27;
+export function logName() {
+	console.log(name);
+}
