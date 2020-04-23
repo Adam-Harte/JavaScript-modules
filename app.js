a6fb4888462f769c5787391193bb03fc64d07be0
Adam-Harte <adamharte11@hotmail.co.uk>
2020-04-23 23:38:46 +0100

    tweak

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ logName();
 
 // combined imports
 import * as info from './combinedImports';
+import { job as occupation } from './combinedImports';
 
 console.log(info.job);
 console.log(info.location);
 console.log(info.hobbies);
+
+console.log(occupation);
